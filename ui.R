63da753a467eedd63bbd763991130dcb80f4bf50
pj.debarros@gmail.com <pj.debarros@gmail.com>
2016-01-17 07:28:32 -0500

    initial commit

---
+++ ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(shinyBS)
+source("TabPanelFlex.R")
+source("uielements.R")
+shinyUI(fluidPage(
+  tagList(header),
+  div(class = "flex1",
+      bsCollapse(id = "top",
+                 bsCollapsePanel(title = "Top Collapse Panel", 
+                                 h3(tags$ul(tags$li("This is Top Collapse Panel."),tags$li("Isn't is pretty?"))))),
+      h2("Middle Stuff"),
+      div(class = "flex2",
+          scrollPanel(style = "width:50%;", 
+                      h1(tags$ul(tags$li(1),tags$li(1),tags$li(1),tags$li(1),tags$li(1),tags$li(1),tags$li(1),tags$li(1),tags$li(1),tags$li(1)))),
+          div(style = "width:50%; background-color:pink; border: 5px solid purple;",
+              h3("Other Stuff"))
+      )
+  )
+))
---
+++ uielements.R
@@ -0,0 +1,31 @@
+header = list(tags$head(tags$style(HTML("
+.flex1{
+display: -webkit-flex;
+display: flex;
+-webkit-flex-direction: column;
+flex-direction: column;
+height: 100vh;
+max-height: 100%;
+border: 5px solid red; 
+}
+.flex2{
+display: -webkit-flex;
+display: flex;
+-webkit-flex-direction: row;
+flex-direction: row; 
+height: 100%;
+max-height: 100%;
+background-color: yellow; 
+overflow-y:hidden;
+}
+.scrollpanel{
+overflow-y: scroll; 
+position: relative; 
+margin: 0px; 
+padding: 0px; 
+border: 0px;
+}
+"))))
+
+scrollPanel = function(...){
+  div(class = "well scrollpanel", ...)}
